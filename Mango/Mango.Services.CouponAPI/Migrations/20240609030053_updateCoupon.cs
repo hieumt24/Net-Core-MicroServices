2557e335f2019c31e2a025f02819dcbbd8124bf6
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-06-09 10:46:01 +0700

    add file mango

---
+++ Mango/Mango.Services.CouponAPI/Migrations/20240609030053_updateCoupon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateCoupon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Coupons");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Coupons",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1,
+                column: "LastUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2,
+                column: "LastUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
